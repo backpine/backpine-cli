f81d3b4dfc53b346b6c65ece7b79fcee816d0fd4
Matthew Sessions <matthewlsessions@gmail.com>
2025-04-21 14:36:31 -0600

    release v1

--- ttt/worker/trpc/router.ts
+++
@@ -1,8 +0,0 @@
-import { t } from "@worker/trpc/trpc-instance";
-import { exampleTrpcRoutes } from "./routers/example";
-
-export const appRouter = t.router({
-  exampleTrpc: exampleTrpcRoutes,
-});
-
-export type AppRouter = typeof appRouter;
